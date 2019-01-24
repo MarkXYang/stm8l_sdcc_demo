0b6ad8f0963c470e864cb4e36dd1bae375104c41
Mark Yang <mark.xc.yang@gmail.com>
2019-01-23 22:59:29 -0500

    Change to green led flash

--- ledblink.c
+++ ledblink.c
@@ -5,11 +5,11 @@ int main()
 {
     CLK_DIVR = 0x00; /* Set Clock to 16MHz */
 
-    PC_DDR = 0x80;
-    PC_CR1 = 0x80;
+    PE_DDR = 0x80;
+    PE_CR1 = 0x80;
 
     while(1) {
-        PC_ODR ^= 0x80;
+        PE_ODR ^= 0x80;
 	delay_ms(500);
     }
 }
